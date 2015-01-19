15f1a041c7ef03d565df6d66b2781db296f585b6
CathyHuangtw <cathyhaung.tw@gmail.com>
2015-01-19 10:58:46 +0800

    Finish Static Pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,20 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home - Cathy's App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help - Cathy's App"
   end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About - Cathy's App"
+  end
+
 
 end
